add9ebbc95dac78c1001fe8f88765c157d4260ea
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-06-07 02:25:32 +0900

    Fix className

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout/layout"
 import MainImage from "../components/MainImage"
 import SEO from "../components/seo"
 
@@ -9,77 +9,55 @@ const IndexPage = () => (
     <Layout>
         <SEO title="ホーム" />
         <MainImage />
-        <div id="about" className="topPageContents">
-            <h2>テレラボとは？</h2>
-            <p className="center">プログラミングを通して、理科について学びを深めることができる、オンラインの教室です。
-            </p>
-            <div className="card">
-                <h3>初心者でも楽しく、そして深くプログラミングを学べる教室です。</h3>
-                <div className="seeMoreWrap">
-                    <input type="checkbox" id="seeMoreTriggerAbout" className="seeMoreTrigger" />
-                    <label htmlFor="seeMoreTriggerAbout" className="seeMoreButton"></label>
-                    <div className="seeMoreContent">
-                        <p>テレラボは「プログラミングをはじめてみたい方」や「プログラミング活動の幅を拡げていきたい方」などのプログラミング学習に関する「はじめの一歩」をサポートいたします。</p>
-                    </div>
-                </div>
 
-            </div>
+        <div id="about" className="sections">
+            <h2 className="title">テレラボとは？</h2>
+            <p className="text">
+                「テレラボ」はインターネットを通して理科とプログラミングが学べるオンラインの教室です。<br />
+            ３つのプランからお子さまに合わせたカタチをお選びいただくことができるため、<br />柔軟な学びを得ることができます。<br />
+            一人ひとりによりそった授業内容で、理科・プログラミングの楽しさを知ってもらうこと。<br />それが私たちのミッションです。</p>
         </div>
-        <div id="cource" className="topPageContents">
-            <h2>選べる2つのコース</h2>
-            <p className="center">
+        <div id="cource" className="sections">
+            <h2 className="title">選べる2つのコース</h2>
+            <p className="text">
                 近日中に他にもコース追加予定！</p>
             <div className="cards">
                 <div className="card">
-                    <h3>無料プラン</h3>
-                    <span>全1回　￥6,000</span>
-                    <p>
-                        開発を安全かつ効率的に進めることができる環境を学ぶコースです。
-          </p>
-                    <Link to="modernEnvDev">
+                    <h3 className="card__title">自習室コース</h3>
+                    <p className="card__description">
+                        オンラインの自習室が使い放題です。<br />
+                        他の人とのコミュニケーションも行うことができます。</p>
+                    <p className="card__price">0</p>
+                    {/* <Link to="modernEnvDev">
                         <div className="button">
                             詳細はこちら</div>
-                    </Link>
+                    </Link> */}
                 </div>
                 <div className="card">
-                    <h3>無料プラン</h3>
-                    <span>全1回　￥6,000</span>
-                    <p>
-                        開発を安全かつ効率的に進めることができる環境を学ぶコースです。
-          </p>
-                    <Link to="modernEnvDev">
+                    <h3 className="card__title">自習室コース</h3>
+                    <p className="card__description">
+                        オンラインの自習室が使い放題です。<br />
+                        他の人とのコミュニケーションも行うことができます。</p>
+                    <p className="card__price">0</p>
+                    {/* <Link to="modernEnvDev">
                         <div className="button">
                             詳細はこちら</div>
-                    </Link>
-                </div>
-                <div className="card">
-                    <h3>無料プラン</h3>
-                    <span>全1回　￥6,000</span>
-                    <p>
-                        開発を安全かつ効率的に進めることができる環境を学ぶコースです。
-          </p>
-                    <Link to="modernEnvDev">
-                        <div className="button">
-                            詳細はこちら</div>
-                    </Link>
+                    </Link> */}
                 </div>
             </div>
-
-
         </div>
-        <div id="community" className="topPageContents">
-            <h2>開講情報とお申し込みはこちら！</h2>
-            <div className="center">
+        <div id="community" className="sections">
+            <h2 className="title">開講情報とお申し込みはこちら！</h2>
+            <p className="text"></p>
 
-            </div>
             {/* <h2>まずはテレラボSlackコミュニティに参加しよう！</h2> */}
             {/* <p className="center">コミュニティへの参加は無料！</p> */}
         </div>
-        <div id="team" className="topPageContents">
-            <h2>運営会社</h2>
-            <p className="center">
+        <div id="team" className="sections">
+            <h2 className="title">運営会社</h2>
+            <p className="text">
                 運営会社
-      </p>
+            </p>
             <div className="cards">
                 <div className="card">
                     <div className="profile">
